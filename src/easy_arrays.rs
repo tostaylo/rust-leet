bf2b42fa7af5c5405033b15675231ddf7c83dab7
Torre <tostaylo1@gmail.com>
2019-12-28 14:01:27 -0600

    removing warnings for unused functions in test.rs

--- src/easy_arrays.rs
+++ src/easy_arrays.rs
@@ -1,6 +1,7 @@
 use std::cmp;
 use std::collections::hash_map::Entry::{Occupied, Vacant};
 use std::collections::HashMap;
+
 //Requirements: Modify in-place with 0(1) space complexity
 pub fn remove_duplicates_sorted_array(nums: &mut Vec<i32>) -> i32 {
   if nums.len() > 0 {
@@ -13,34 +14,34 @@ pub fn remove_duplicates_sorted_array(nums: &mut Vec<i32>) -> i32 {
   nums.len() as i32
 }
 
-pub fn max_profit(prices: Vec<i32>) -> i32 {
-  let mut total = 0;
-  for index in 0..prices.len() - 1 {
-    let start_day = index;
-    let mut end_day = start_day + 1;
-    let mut largest = 0;
-    while end_day <= prices.len() - 1 {
-      if prices[start_day] >= prices[end_day] {
-        end_day = end_day + 1;
-      } else {
-        let diff = prices[end_day] - prices[start_day];
-        println!(
-          "start {:?} end {:?}  largest {:?} ",
-          prices[start_day], prices[end_day], largest
-        );
-        if diff > largest {
-          largest = diff;
-          end_day = end_day + 1;
-          total = total + largest;
-          println!("{:?} ", largest);
-        } else {
-          end_day = end_day + 1;
-        }
-      }
-    }
-  }
-  total
-}
+// pub fn max_profit(prices: Vec<i32>) -> i32 {
+//   let mut total = 0;
+//   for index in 0..prices.len() - 1 {
+//     let start_day = index;
+//     let mut end_day = start_day + 1;
+//     let mut largest = 0;
+//     while end_day <= prices.len() - 1 {
+//       if prices[start_day] >= prices[end_day] {
+//         end_day = end_day + 1;
+//       } else {
+//         let diff = prices[end_day] - prices[start_day];
+//         println!(
+//           "start {:?} end {:?}  largest {:?} ",
+//           prices[start_day], prices[end_day], largest
+//         );
+//         if diff > largest {
+//           largest = diff;
+//           end_day = end_day + 1;
+//           total = total + largest;
+//           println!("{:?} ", largest);
+//         } else {
+//           end_day = end_day + 1;
+//         }
+//       }
+//     }
+//   }
+//   total
+// }
 
 // O(n) Time and Space complexity
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
--- src/tests.rs
+++ src/tests.rs
@@ -1,24 +1,26 @@
-use crate::easy_arrays::{
-  intersect, max_profit, move_zeroes, remove_duplicates_sorted_array, rotate_array, rotate_image,
-  single_number, two_sum,
-};
-use crate::easy_strings::{first_uniq_char, is_anagram, is_palindrome, reverse_string, str_str};
 // Note this useful idiom: importing names from outer (for mod tests) scope.
 //  use super::*;
 
 // Easy Arrays
 #[test]
 fn test_remove_duplicates_sorted_array() {
+  #[cfg(test)]
+  use crate::easy_arrays::remove_duplicates_sorted_array;
+
   assert_eq!(
     remove_duplicates_sorted_array(&mut vec![0, 0, 1, 1, 1, 2, 2, 3, 3, 4]),
     5
   );
+
   assert_eq!(remove_duplicates_sorted_array(&mut vec![0, 0, 1]), 2);
   assert_eq!(remove_duplicates_sorted_array(&mut vec![1, 1]), 1);
 }
 
 #[test]
 fn test_two_sum() {
+  #[cfg(test)]
+  use crate::easy_arrays::two_sum;
+
   assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
   assert_eq!(two_sum(vec![2, 7, 11, 15], 26), vec![2, 3]);
   assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
@@ -26,6 +28,9 @@ fn test_two_sum() {
 
 #[test]
 fn test_rotate_array() {
+  #[cfg(test)]
+  use crate::easy_arrays::rotate_array;
+
   let mut test_collection = vec![1, 2, 3, 4, 5, 6, 7];
   rotate_array(&mut test_collection, 3);
   assert_eq!(test_collection, vec![5, 6, 7, 1, 2, 3, 4]);
@@ -34,48 +39,63 @@ fn test_rotate_array() {
   assert_eq!(test_collection2, vec![2, 1]);
 }
 
+#[test]
 fn test_single_number() {
+  #[cfg(test)]
+  use crate::easy_arrays::single_number;
+
   assert_eq!(single_number(vec![2, 2, 1]), 1);
   assert_eq!(single_number(vec![4, 1, 2, 1, 2]), 4);
 }
 
 #[test]
 fn test_intersect() {
+  #[cfg(test)]
+  use crate::easy_arrays::intersect;
+
   assert_eq!(intersect(vec![1, 2, 2, 1], vec![2, 2]), [2, 2]);
 }
 
 #[test]
 fn test_move_zeroes() {
+  #[cfg(test)]
+  use crate::easy_arrays::move_zeroes;
+
   let mut tester_vec = vec![0, 1, 0, 3, 12];
   move_zeroes(&mut tester_vec);
   assert_eq!(tester_vec, vec![1, 3, 12, 0, 0]);
 }
 
-// #[test]
-// fn test_rotate_image() {
-//   let mut tester = vec![
-//     vec![43, 39, 3, 33, 37, 20, 14],
-//     vec![9, 18, 9, -1, 40, 22, 38],
-//     vec![14, 42, 3, 23, 12, 14, 32],
-//     vec![18, 31, 45, 11, 8, -1, 31],
-//     vec![28, 44, 14, 23, 40, 24, 13],
-//     vec![29, 45, 33, 45, 20, 0, 45],
-//     vec![12, 23, 35, 32, 22, 39, 8],
-//   ];
-//   rotate_image(&mut tester);
-//   assert_eq!(
-//     tester,
-//     vec![
-//       vec![12, 29, 28, 18, 14, 9, 43],
-//       vec![23, 45, 44, 31, 42, 18, 39],
-//       vec![35, 33, 14, 45, 3, 9, 3],
-//       vec![32, 45, 23, 11, 23, -1, 33],
-//       vec![22, 20, 40, 8, 12, 40, 37],
-//       vec![39, 0, 24, -1, 14, 22, 20],
-//       vec![8, 45, 13, 31, 32, 38, 14]
-//     ]
-//   );
-// }
+#[test]
+fn test_rotate_image() {
+  #[cfg(test)]
+  use crate::easy_arrays::rotate_image;
+  let mut tester = vec![
+    vec![43, 39, 3, 33, 37, 20, 14],
+    vec![9, 18, 9, -1, 40, 22, 38],
+    vec![14, 42, 3, 23, 12, 14, 32],
+    vec![18, 31, 45, 11, 8, -1, 31],
+    vec![28, 44, 14, 23, 40, 24, 13],
+    vec![29, 45, 33, 45, 20, 0, 45],
+    vec![12, 23, 35, 32, 22, 39, 8],
+  ];
+  rotate_image(&mut tester);
+  assert_eq!(
+    tester,
+    vec![
+      vec![12, 29, 28, 18, 14, 9, 43],
+      vec![23, 45, 44, 31, 42, 18, 39],
+      vec![35, 33, 14, 45, 3, 9, 3],
+      vec![32, 45, 23, 11, 23, -1, 33],
+      vec![22, 20, 40, 8, 12, 40, 37],
+      vec![39, 0, 24, -1, 14, 22, 20],
+      vec![8, 45, 13, 31, 32, 38, 14]
+    ]
+  );
+  // let mut tester = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+  // rotate_image(&mut tester);
+  // assert_eq!(tester, vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3]]);
+}
 
 // #[test]
 // fn test_max_profit() {
@@ -86,12 +106,20 @@ fn test_move_zeroes() {
 
 #[test]
 fn test_is_palindrome() {
+  #[cfg(test)]
+  use crate::easy_strings::is_palindrome;
+
   assert_eq!(
     is_palindrome("A man, a plan, a canal: Panama".to_owned()),
     true
   );
 }
+
+#[test]
 fn test_reverse_string() {
+  #[cfg(test)]
+  use crate::easy_strings::reverse_string;
+
   let mut hello: Vec<char> = "hello".chars().collect();
   let mut goodbye: Vec<char> = "goodbye".chars().collect();
   reverse_string(&mut hello);
@@ -102,6 +130,9 @@ fn test_reverse_string() {
 
 #[test]
 fn test_is_anagram() {
+  #[cfg(test)]
+  use crate::easy_strings::is_anagram;
+
   assert_eq!(is_anagram("aavb".to_owned(), "bb".to_owned()), false);
   assert_eq!(is_anagram("rat".to_owned(), "art".to_owned()), true);
   assert_eq!(is_anagram("rating".to_owned(), "artink".to_owned()), false);
@@ -110,12 +141,18 @@ fn test_is_anagram() {
 
 #[test]
 fn test_str_str() {
+  #[cfg(test)]
+  use crate::easy_strings::str_str;
+
   assert_eq!(str_str(String::from("hello"), String::from("ll")), 2);
   assert_eq!(str_str(String::from("aaaa"), String::from("ba")), -1);
 }
 
 #[test]
 fn test_first_uniq_char() {
+  #[cfg(test)]
+  use crate::easy_strings::first_uniq_char;
+
   assert_eq!(first_uniq_char("loveleetcode".to_owned()), 2);
   assert_eq!(first_uniq_char("cc".to_owned()), -1);
 }
