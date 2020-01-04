bcd0399e635bcbec0e9460839fc4097fcb9477a5
Torre <tostaylo1@gmail.com>
2020-01-04 15:39:41 -0600

    adding generic performance function

--- src/performance.rs
+++ src/performance.rs
@@ -1,7 +1,16 @@
 use std::time::Instant;
 
 // Can this be made more generic?
-pub fn compare_perf(functions: Vec<fn(Vec<i32>) -> Vec<i32>>, arg: Vec<i32>, iterations: i64) {
+// Or do I need to make a seperate test function for each function parameter and return type
+// Do I instead need to create a struct which compares has methods for compare_perF_vec, compare_perf_string, etc?
+// Or do I cast the function being passed in as a specific type I create? Or implements a trait I create?
+// Many possibilities here.
+// functions: Vec<fn(Vec<i32>) -> Vec<i32>> this works but isn't flexible
+// generic type where T implements a clone trait?
+// If I figure this out, this could be a blog post
+// So now I have it for Clone trait but do strings implement Clone trait?
+// May still need to make a struct for this with methods for strings vs vecs
+pub fn compare_perf<T: Clone>(functions: Vec<fn(T) -> T>, arg: T, iterations: i64) {
   for (item_number, item) in functions.iter().enumerate() {
     let now = Instant::now();
     for _index in 0..iterations {
